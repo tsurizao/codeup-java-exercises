53ceb350bd7e597aede5a662653e886e8b53a080
Chase Martinez <chasemartinez22@gmail.com>
2022-06-06 10:52:39 -0500

    feat completed exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,27 +1,45 @@
+import javax.crypto.spec.PSource;
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
         String piStatement = "The value of pi is approximately ";
-        System.out.printf("%s %.2f%n", piStatement, pi);
+        System.out.format("%s %.2f%n", piStatement, pi);
 
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter an integer: ");
-//        int nextInt = scanner.nextInt();
-//        System.out.println(nextInt);
+        scanner.useDelimiter("\n");
+        System.out.println("Enter an integer: ");
+        int nextInt = scanner.nextInt();
+        System.out.println(nextInt);
 
-//        System.out.println("Enter a word: ");
-//        String firstWord = scanner.next();
-//        System.out.println("Enter another word: ");
-//        String secondWord = scanner.next();
-//        System.out.println("Enter a final word: ");
-//        String thirdWord = scanner.next();
-//        System.out.printf("%n%s %n%s %n%s", firstWord, secondWord, thirdWord);
+        System.out.println("Enter a word: ");
+        String firstWord = scanner.next();
+        System.out.println("Enter another word: ");
+        String secondWord = scanner.next();
+        System.out.println("Enter a final word: ");
+        String thirdWord = scanner.next();
+        System.out.printf("%n%s %n%s %n%s", firstWord, secondWord, thirdWord);
+        System.out.println("");
+
 
         System.out.println("Enter a sentence: ");
-        String mySentence = scanner.next();
-
+        String mySentence = scanner.nextLine();
         System.out.println(mySentence);
+
+        System.out.println("Calculate the perimeter and area of this room.");
+        System.out.println("Enter the length of the room: ");
+        float length = scanner.nextFloat();
+        System.out.println("Enter the width of the room: ");
+        float width = scanner.nextFloat();
+        System.out.println("Enter the height of the room: ");
+        float height = scanner.nextFloat();
+        System.out.println("The perimeter of the room is " + (length * 2 + width * 2));
+        System.out.println("The area of the room is " + (length * width));
+        System.out.println("The volume of the room is " + (length * width * height));
+
+        System.out.println(Integer.MAX_VALUE);
+
     }
 }
