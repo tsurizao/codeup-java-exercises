0e1a6dae950539ea983333701e36d20c8950d5e3
Chase Martinez <chasemartinez22@gmail.com>
2022-06-07 10:28:23 -0500

    Add exercises 1 completion

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,17 +5,15 @@ public class ControlFlowExercises {
 //        For all of the following exercises, create a new class named ControlFlowExercises with a main method. After each exercise, commit your changes, then replace your code in the main method with the next exercise.
 
         //1
-        int i = 5;
-        while (i <= 15) {
+
+        //A
+        for (int i = 5; i <= 15; i++) {
             System.out.println(i);
-            i++;
         }
 
-        int num = 4;
-        do {
-            System.out.println(num);
-//            num *= num; // causes an issue when starting value is 2 or 4
-            num = (int) Math.pow(num, 2);
-        } while (num < 1000000);
+        //B
+        for (int i = 2; i < 1000000; i *= i) {
+            System.out.println(i);
+        }
     }
 }
