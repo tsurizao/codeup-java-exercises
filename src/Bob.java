27b224d8219f786d655ef6277ed88b4e44cb598b
Chase Martinez <chasemartinez22@gmail.com>
2022-06-08 16:57:04 -0500

    feat new timing loops and system.out strings

--- src/Bob.java
+++ src/Bob.java
@@ -4,17 +4,46 @@ public class Bob {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        long startTime = System.currentTimeMillis();
+        System.out.println("\n \n \n Loading BobChat v0.834-beta4323......");
+        while (true) {
+            if ((System.currentTimeMillis() - startTime) / 1000 == 3) {
+                break;
+            }
+        }
+        while (true) {
+            if ((System.currentTimeMillis() - startTime) / 1000 == 5) {
+                break;
+            }
+            System.out.println("..........boop...");
+            System.out.println("....beep..................");
+        }
+        System.out.println("\n \n \n Success. \n \n \n");
+        System.out.println("Welcome to BobChat, Bob is your best friendly neighborhood lackadaisical teenager.");
         System.out.println("Talk to Bob, ask him a question, yell at him or say nothing at all.");
+        System.out.println("You can also say \"done here\" to exit the program. \n");
+
+
         while (true) {
             String input = sc.nextLine();
-            if (input.endsWith("?")){
+            if (input.endsWith("?")) {
                 System.out.println("Bob: 'Sure.'");
-            }
-            else if (input.endsWith("!")){
+            } else if (input.endsWith("!")) {
                 System.out.println("Bob: 'Whoa, chill out!'");
-            }
-            else if (input.trim().equals("")){
+            } else if (input.trim().equals("")) {
                 System.out.println("Bob: 'Fine. Be that way!'");
+                // The next line of code was added for fun, if there are any numbers in the string Bob will have a different response
+            } else if (input.contains("0") || input.contains("1") || input.contains("2") || input.contains("3") || input.contains("4") || input.contains("5") || input.contains("6") || input.contains("7") || input.contains("8") || input.contains("9")) {
+                System.out.println("Bob: 'Dude, numbers, gross.'");
+            } else if (input.contains("done here")) {
+                System.out.println("Bob: 'Smell ya later.'");
+                long exitTime = System.currentTimeMillis();
+                while (true) {
+                    if ((System.currentTimeMillis() - exitTime) / 1000 == 2) {
+                        break;
+                    }
+                }
+                System.out.println("Exiting BobChat v0.834-beta4323, have a nice day!");
             } else {
                 System.out.println("Bob: 'Whatever.'");
             }
