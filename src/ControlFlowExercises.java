ec6ca0a6304259078cdea48e5c09da8046a23161
Chase Martinez <chasemartinez22@gmail.com>
2022-06-08 15:27:54 -0500

    refactored exercise 3 to make use of formatted strings

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,59 +4,26 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
         //3
+
         Scanner s = new Scanner(System.in);
-
-        //Initial values to allow program to run properly
-        int userInput = 0;
+        System.out.println("What number would you like to go up to?");
+        int userInput = s.nextInt();
+        int i = 0;
         String userResponse = "";
-
-        // Asks for input, and forms the upper part of table
-        while (userInput < 1 || userInput > 100) {
-            System.out.println("Choose a number between 1 and 100?");
-            userInput = s.nextInt();
-        }
-        // Out put the top of the table
-        System.out.println("Here is your table!\n\nnumber | squared | cubed\n-------|---------|-------\n1      |1        |1      ");
-
-        // Outputs input into table like format
-        for (int i = 2; i <= 100; i++) {
-            // Asks user if they would like to continue with the rest of the table once their input was satisfied
-            if (i > userInput) {
-                System.out.println("Would you like to continue? Enter 'Yes' or 'No'");
-                userResponse = s.next().toLowerCase();
-            }
-            // Exits loop if uses wishes to stop
-            if (userResponse.equals("no")) {
-                System.out.println("Loop exited");
-                break;
-            }
-            // Continues through the rest of the loop if user wishes to continue
-            if (userResponse.equals("yes")) {
-                userInput = 100;
-            }
+        System.out.println("number | squared | cubed\n-------|---------|-------");
+        do {
+            i++;
             int squared = i * i;
             int cubed = i * i * i;
-            String iSpaces = "      |";
-            String squaredSpaces = "        |";
-
-            // changes spaces depending on number of digits in i
-            if (i > 9 & i <= 99) {
-                iSpaces = "     |";
-            } else if (i > 99) {
-                iSpaces = "    |";
+            System.out.printf("%-6d | %-7d | %-5d%n", i, squared, cubed);
+            if (i == userInput) {
+                System.out.println("Would you like to continue?");
+                userResponse = s.next().toLowerCase();
+                if (userResponse.equals("no")) {
+                    System.out.println("Exiting Loop.");
+                    break;
+                }
             }
-            // changes spaces depending on number of digits in squared integer
-            if (squared > 9 && squared <= 99) {
-                squaredSpaces = "       |";
-            } else if (squared > 99 && squared <= 999) {
-                squaredSpaces = "      |";
-            } else if (squared > 999 && squared <= 9999) {
-                squaredSpaces = "     |";
-            } else if (squared > 9999) {
-                squaredSpaces = "    |";
-            }
-            System.out.println(i + iSpaces + squared + squaredSpaces + cubed);
-        }
-
+        } while (i <= 99);
     }
 }
