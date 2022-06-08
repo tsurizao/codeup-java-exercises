3f876904ef68dc5cc675a0dc8dbdd2520c190479
Chase Martinez <chasemartinez22@gmail.com>
2022-06-08 15:09:06 -0500

    Refactor exercise 3 to save the entire top table string into one variable

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,48 +1,62 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-//        Exercises
-//        For all of the following exercises, create a new class named ControlFlowExercises with a main method. After each exercise, commit your changes, then replace your code in the main method with the next exercise.
-
-        //4
+        //3
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter a numerical grade value from 0 to 100");
-        int userGrade = s.nextInt();
-        System.out.println("Would you like to see your letter grade?  Please enter 'Yes' or 'No'");
-        String userContinue = s.next().toLowerCase();
-        if (userContinue.equals("yes")) {
-            if (userGrade > 97){
-                System.out.println("Your grade is an A+");
-            } else if (userGrade > 93) {
-                System.out.println("Your grade is an A");
-            } else if (userGrade > 89) {
-                System.out.println("Your grade is an A-");
-            } else if (userGrade > 87) {
-                System.out.println("Your grade is a B+");
-            } else if (userGrade > 83) {
-                System.out.println("Your grade is a B");
-            } else if (userGrade > 79) {
-                System.out.println("Your grade is a B-");
-            } else if (userGrade > 77) {
-                System.out.println("Your grade is a C+");
-            } else if (userGrade > 73) {
-                System.out.println("Your grade is a C");
-            } else if (userGrade > 69) {
-                System.out.println("Your grade is a C-");
-            } else if (userGrade > 67) {
-                System.out.println("Your grade is a D+");
-            } else if (userGrade > 63) {
-                System.out.println("Your grade is a D");
-            } else if (userGrade > 59) {
-                System.out.println("Your grade is a D-");
-            } else {
-                System.out.println("Your grade is an F");
-            }
-        } else {
-            System.out.println("No problem, have a good day!");
+
+        //Initial values to allow program to run properly
+        int userInput = 0;
+        String userResponse = "";
+
+        // Asks for input, and forms the upper part of table
+        while (userInput < 1 || userInput > 100) {
+            System.out.println("Choose a number between 1 and 100?");
+            userInput = s.nextInt();
         }
+        // Out put the top of the table
+        System.out.println("Here is your table!\n\nnumber | squared | cubed\n-------|---------|-------\n1      |1        |1      ");
+
+        // Outputs input into table like format
+        for (int i = 2; i <= 100; i++) {
+            // Asks user if they would like to continue with the rest of the table once their input was satisfied
+            if (i > userInput) {
+                System.out.println("Would you like to continue? Enter 'Yes' or 'No'");
+                userResponse = s.next().toLowerCase();
+            }
+            // Exits loop if uses wishes to stop
+            if (userResponse.equals("no")) {
+                System.out.println("Loop exited");
+                break;
+            }
+            // Continues through the rest of the loop if user wishes to continue
+            if (userResponse.equals("yes")) {
+                userInput = 100;
+            }
+            int squared = i * i;
+            int cubed = i * i * i;
+            String iSpaces = "      |";
+            String squaredSpaces = "        |";
+
+            // changes spaces depending on number of digits in i
+            if (i > 9 & i <= 99) {
+                iSpaces = "     |";
+            } else if (i > 99) {
+                iSpaces = "    |";
+            }
+            // changes spaces depending on number of digits in squared integer
+            if (squared > 9 && squared <= 99) {
+                squaredSpaces = "       |";
+            } else if (squared > 99 && squared <= 999) {
+                squaredSpaces = "      |";
+            } else if (squared > 999 && squared <= 9999) {
+                squaredSpaces = "     |";
+            } else if (squared > 9999) {
+                squaredSpaces = "    |";
+            }
+            System.out.println(i + iSpaces + squared + squaredSpaces + cubed);
+        }
+
     }
 }
