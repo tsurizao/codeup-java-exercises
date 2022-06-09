51803c15a5503309e28273548d0eaef428777f6d
Chase Martinez <chasemartinez22@gmail.com>
2022-06-09 09:19:12 -0500

    fixed spelling mistake and added extra line to final string variable

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -5,7 +5,7 @@ public class StringExercise {
         String str1 = "We don't need no education\nWe don't need no thought control";
         String str2 = "Check \"this\" out!, \"s inside of \"s!";
         String str3 = "In windows, the main drive is usually C:\\";
-        String str4 = "I can do backslashes \\, double backslashes \\\\, annd he amazing triple backslash\\\\\\!";
+        String str4 = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash\\\\\\!";
 
         System.out.println(str1);
         System.out.println(str2);
