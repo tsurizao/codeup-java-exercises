6207570ab6db35e745d48663cb7c519f17113176
Chase Martinez <chasemartinez22@gmail.com>
2022-06-09 09:43:30 -0500

    feat exercise 1 completion

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -1,2 +1,31 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class MethodExercises {
+    public static void main(String[] args) {
+        System.out.println(addition(5, 3));
+        System.out.println(subtraction(5, 3));
+        System.out.println(multiplication(5, 3));
+        System.out.println(division(5, 3));
+        System.out.println(modulus(5, 3));
+    }
+
+    public static int addition(int num, int num2) {
+        return num + num2;
+    }
+
+    public static int subtraction(int num, int num2) {
+        return num - num2;
+    }
+
+    public static int multiplication(int num, int num2) {
+        return num * num2;
+    }
+
+    public static int division(int num, int num2) {
+        return num / num2;
+    }
+
+    public static int modulus(int num, int num2) {
+        return num % num2;
+    }
 }
