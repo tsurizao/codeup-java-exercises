8afed36fe1f7548912475b639e92e4244ac410c6
Chase Martinez <chasemartinez22@gmail.com>
2022-06-09 14:20:00 -0500

    feat performant solution to exercise 4, accounts for mistyping the word roll

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class MethodExercises {
     public static void main(String[] args) {
-        System.out.println("Addition: " + add(5, 3));
-        System.out.println("Subtraction: " + subtract(5, 3));
-        System.out.println("Division: " + divide(5, 3));
-        System.out.println("Modulus: " + modulo(5, 3));
-        System.out.println("Multiplication: " + multiply(5, 3));
-        System.out.println("Multiplication Loop: " + multiplicationLoopA(5, 3));
-        System.out.println("Multiplication Loop: " + multiplicationLoopB(5, 3));
-        System.out.println("Multiplication Recursion: " + multiplicationRecursion(5, 3));
-        numberFactorial(1, 20);
+//        System.out.println("Addition: " + add(5, 3));
+//        System.out.println("Subtraction: " + subtract(5, 3));
+//        System.out.println("Division: " + divide(5, 3));
+//        System.out.println("Modulus: " + modulo(5, 3));
+//        System.out.println("Multiplication: " + multiply(5, 3));
+//        System.out.println("Multiplication Loop: " + multiplicationLoopA(5, 3));
+//        System.out.println("Multiplication Loop: " + multiplicationLoopB(5, 3));
+//        System.out.println("Multiplication Recursion: " + multiplicationRecursion(5, 3));
+//        numberFactorial(1, 20);
+        diceRoll();
     }
 
+    //1
+    //Create basic calculation methods
     public static int add(int num, int num2) {
         return num + num2;
     }
@@ -33,7 +36,7 @@ public class MethodExercises {
         return num * num2;
     }
 
-    //using loop to calculate product without using *
+    //(bonus exercise) using loop to calculate product without using *
     public static int multiplicationLoopA(int num, int num2) {
         int initialValue = num;
         for (int i = 1; i < num2; num2--) {
@@ -42,7 +45,7 @@ public class MethodExercises {
         return num;
     }
 
-    //using loop to calculate product without using *
+    //(bonus exercise) using loop to calculate product without using *
     public static int multiplicationLoopB(int num, int num2) {
         int product = 0;
         for (int i = 0; i < num2; i++) {
@@ -51,12 +54,13 @@ public class MethodExercises {
         return product;
     }
 
-    // using recursion to calculate product without using *
+    //(bonus exercise) using recursion to calculate product without using *
     public static int multiplicationRecursion(int num, int num2) {
         return num2 <= 1 ? num : num + multiplicationRecursion(num, num2 - 1);
     }
 
-    // Uses recursion to get a correct response from scanner input
+    //2
+    //Uses recursion to get a correct response from scanner input
     public static int getInteger(int min, int max) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter a valid number between " + min + " and " + max);
@@ -68,25 +72,55 @@ public class MethodExercises {
         }
     }
 
-    // factorial of a number
+    //3
+    // Calculate the factorial of a number
+    // Used previous method to verify integer meets criteria
     public static void numberFactorial(int min, int max) {
         while (true) {
             int userInput = getInteger(min, max);
-            long modifiedInput = 1;
+            long factorialProduct = 1;
             for (int i = userInput; i > 0; i--) {
-                modifiedInput *= i;
+                factorialProduct *= i;
             }
-            System.out.println(modifiedInput);
+            System.out.println(factorialProduct);
             System.out.println("Would you like to continue? (y/n)");
             Scanner sc = new Scanner(System.in);
             String userResponse = sc.next().toLowerCase();
-            if(userResponse.equals("n")){
+            if (userResponse.equals("n")) {
                 System.out.println("Exiting Loop!");
                 break;
-            } else if (userResponse.equals("y")){
-                System.out.println("Continuing");
+            } else if (userResponse.equals("y")) {
                 numberFactorial(min, max);
             }
+        }
+    }
+
+    //4
+    // Simulate a pair of dice rolling
+    public static void diceRoll() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Let's roll some dice!");
+        System.out.println("Enter the number of sides for a pair of dice.");
+        int numberOfSides = sc.nextInt();
+        while (true) {
+            System.out.println("Type 'roll' to roll your dice.");
+            String roll = sc.next().toLowerCase();
+            if (roll.equals("roll")) {
+                while (true) {
+                    System.out.println("Die 1: " + ((int) Math.floor(Math.random() * (numberOfSides - 1 + 1)) + 1));
+                    System.out.println("Die 2: " + ((int) Math.floor(Math.random() * (numberOfSides - 1 + 1)) + 1));
+                    System.out.println("Would you like to roll again? (y/n)");
+                    String again = sc.next().toLowerCase();
+                    if (again.equals("n")) {
+                        break;
+                    }
+
+                }
+            } else {
+                continue;
+            }
+            System.out.println("Thanks for playing!");
+            break;
         }
     }
 }
