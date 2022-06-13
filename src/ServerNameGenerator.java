19d7ad7e093e9e0b9ad671e353c2610d4d4d0b3c
Chase Martinez <chasemartinez22@gmail.com>
2022-06-13 18:28:31 -0500

    Feat exercise 2 completion

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,14 @@
+public class ServerNameGenerator {
+
+    public static String serverNameGenerator() {
+        String[] adjectiveArr = {"Spunky", "Hungry", "Silly", "Sassy", "Sneaky", "Energetic", "Quiet", "Boisterous", "Vengeful", "Lazy"};
+        String[] nounArr = {"Kitten", "Walrus", "Frog", "Cobra", "Mustang", "Hippo", "Eagle", "Monkey", "Sloth", "Ocelot"};
+        int randomAdjective = (int) (Math.random() * 10);
+        int randomNoun = (int) (Math.random() * 10);
+        return (adjectiveArr[randomAdjective] + "-" + nounArr[randomNoun]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Here is your server name:\n" + serverNameGenerator());
+    }
+}
