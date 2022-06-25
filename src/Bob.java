19ed5aaf9145fc5d055493fbd4c1db92723f44ed
Chase Martinez <chasemartinez22@gmail.com>
2022-06-25 14:49:06 -0500

    Changed exit phrase to "done" instead of "done here" Removed a few unnecessary spaces

--- src/Bob.java
+++ src/Bob.java
@@ -5,7 +5,7 @@ public class Bob {
 
         Scanner sc = new Scanner(System.in);
         long startTime = System.currentTimeMillis();
-        System.out.println("\n \n \n Loading BobChat v0.834-beta4323......");
+        System.out.println("\n\n\nLoading BobChat v0.834-beta4323......");
         while (true) {
             if ((System.currentTimeMillis() - startTime) / 1000 == 3) {
                 break;
@@ -21,7 +21,7 @@ public class Bob {
         System.out.println("\n \n \n Success. \n \n \n");
         System.out.println("Welcome to BobChat, Bob is your best friendly neighborhood lackadaisical teenager.");
         System.out.println("Talk to Bob, ask him a question, yell at him or say nothing at all.");
-        System.out.println("You can also say \"done here\" to exit the program. \n");
+        System.out.println("You can also say \"done\" to exit the program. \n");
 
         while (true) {
             String input = sc.nextLine();
@@ -33,8 +33,8 @@ public class Bob {
                 System.out.println("Bob: 'Fine. Be that way!'");
                 // The next line of code was added for fun, if there are any numbers in the string Bob will have a different response
             } else if (input.contains("0") || input.contains("1") || input.contains("2") || input.contains("3") || input.contains("4") || input.contains("5") || input.contains("6") || input.contains("7") || input.contains("8") || input.contains("9")) {
-                System.out.println("Bob: 'Dude, numbers, gross.'");
-            } else if (input.contains("done here")) {
+                System.out.println("Bob: 'Dude, numbers, I don't do math, man.'");
+            } else if (input.contains("done")) {
                 System.out.println("Bob: 'Smell ya later.'");
                 System.out.println("Exiting BobChat v0.834-beta4323, have a nice day!");
                 break;
